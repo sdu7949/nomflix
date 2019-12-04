f1b8c512a19760b1d6276f6d29c5a2ad18dbc4f2
username <user@email.mail>
2019-12-04 14:14:40 +0900

    Updates

---
+++ precache-manifest.d376b1cd25789aa15ae33fec918ffde2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc67b79c0e7863ab99b89f0f7a18071b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61b090e236603d44152",
+    "url": "/static/js/2.dad18ba8.chunk.js"
+  },
+  {
+    "revision": "9cf4badb50594cceaf8c",
+    "url": "/static/js/main.152b80aa.chunk.js"
+  },
+  {
+    "revision": "398a4adfc1495f6f036a",
+    "url": "/static/js/runtime-main.ad4e3d2e.js"
+  },
+  {
+    "revision": "0f303739ff6bf98ccb37a4178ab68c37",
+    "url": "/static/media/noPosterSmall.0f303739.png"
+  }
+]);
